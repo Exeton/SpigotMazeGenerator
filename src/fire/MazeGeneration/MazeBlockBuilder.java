2e4c8ae69d262e987c218ab6e167ebcf8e1d4423
BuildTools <unconfigured@null.spigotmc.org>
2018-05-22 00:42:57 -0400

    Cleaned up maze tile placement.

--- src/fire/MazeGeneration/MazeBlockBuilder.java
+++ src/fire/MazeGeneration/MazeBlockBuilder.java
@@ -15,40 +15,37 @@ public class MazeBlockBuilder {
         this.buildingGenerator = buildingGenerator;
         this.mazeGenerator = mazeGenerator;
     }
-    //Todo clean up building calls
     public void fillStructureMap(){
-
-        int buildingElevation = buildingGenerator.buildingElevation;
         MazeTile[][] maze = mazeGenerator.generateMaze();
+
         for (int x = 0; x < maze[0].length; x++){
             for (int z = 0; z < maze[1].length; z++){
                 int tileCenterX = 2 * x + 1;
                 int tileCenterZ = 2 * z + 1;
 
-                buildingGenerator.makePillar(new Vector(tileCenterX - 1 , buildingElevation + 1, tileCenterZ - 1), 3);
-                buildingGenerator.setBlockType(new Vector(tileCenterX - 1, buildingElevation, tileCenterZ - 1), Material.STONE);
-                buildingGenerator.setBlockType(new Vector(tileCenterX, buildingElevation, tileCenterZ), Material.STONE);
-                buildingGenerator.setBlockType(new Vector(tileCenterX - 1, buildingElevation, tileCenterZ), Material.STONE);
-                buildingGenerator.setBlockType(new Vector(tileCenterX, buildingElevation, tileCenterZ - 1), Material.STONE);
+                makeTileFloor(x,z);
+                buildingGenerator.makePillar(tileCenterX - 1 , tileCenterZ - 1, 3);//Corner of cell
+
                 if (maze[x][z].hasWall(DirectionUtil.Direction.North)){
-                    buildingGenerator.makePillar(new Vector(tileCenterX , buildingElevation + 1, tileCenterZ - 1), 3);
+                    buildingGenerator.makePillar(tileCenterX , tileCenterZ - 1, 3);
                 }
                 if (maze[x][z].hasWall(DirectionUtil.Direction.West)){
-                    buildingGenerator.makePillar(new Vector(tileCenterX - 1, buildingElevation + 1, tileCenterZ), 3);
+                    buildingGenerator.makePillar(tileCenterX - 1,  tileCenterZ, 3);
                 }
             }
-            buildingGenerator.makePillar(new Vector(2 * x, buildingElevation + 1,  2 * maze[1].length), 3);
-            buildingGenerator.makePillar(new Vector(2 * x + 1, buildingElevation + 1,  2 * maze[1].length), 3);
-            buildingGenerator.setBlockType(new Vector(2 * x, buildingElevation, 2 * maze[1].length - 1), Material.STONE);
-            buildingGenerator.setBlockType(new Vector(2 * x + 1, buildingElevation, 2 * maze[1].length - 1), Material.STONE);
-        }
 
-
-        for (int i = 0; i < 2 * maze[0].length + 1; i++){
-            buildingGenerator.makePillar(new Vector(2 * maze[0].length, buildingElevation + 1,  i), 3);
-            buildingGenerator.setBlockType(new Vector(2 * maze[0].length - 1, buildingElevation, i),  Material.STONE);
+            //Make maze perimeter
+            buildingGenerator.makePillar(2 * x, 2 * maze[1].length, 3);
+            buildingGenerator.makePillar(2 * x + 1,2 * maze[1].length, 3);
+            makeTileFloor(x, maze[1].length - 1);
         }
+        //Make maze perimeter
+        buildingGenerator.makeWall(2 * maze[0].length, 0, DirectionUtil.Direction.South, 2 * maze[0].length);
     }
+    private void makeTileFloor(int mazeTileX, int mazeTileZ){
+        buildingGenerator.makeFloor(mazeTileX * 2, mazeTileZ * 2, 2, 2);
+    }
+
     public static int blocksToCells(int blocks){
         return (blocks - 1) / 2;
     }
