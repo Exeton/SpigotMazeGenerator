aa33c5a402903c7bce859e344d82bfdb77623986
Exeton <benjamin.diederich@gmail.com>
2018-10-15 23:47:37 -0400

    Added maze size test case.

---
+++ Test/fire/MazeGeneration/MazeAlgorthim/MazeGeneratorTest.groovy
@@ -0,0 +1,22 @@
+package fire.MazeGeneration.MazeAlgorthim
+
+import org.junit.Assert
+import org.junit.Test
+
+class MazeGeneratorTest extends GroovyTestCase {
+
+
+    @Test
+    void testNextMazeSize() throws Exception{
+        MazeGenerator mazeGenerator = new MazeGenerator(33, 65, new Random());
+        MazeTile[][] maze = mazeGenerator.nextMaze();
+
+        boolean mazeIsProperSize = (maze[0].length == 33) && (maze[1].length == 65);
+        Assert.assertTrue(mazeIsProperSize);
+    }
+
+    void testNextMaze() throws Exception{
+        MazeGenerator mazeGenerator = new MazeGenerator(32, 32, new Random());
+
+    }
+}
